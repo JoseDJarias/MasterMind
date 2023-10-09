fa1f91fb129833f3d1fa84c66cf61390f707037f
JoseDJarias <arias_da044@hotmail.com>
2023-10-09 15:47:17 -0600

    Segundo commit

--- PlayerUser.rb
+++ PlayerUser.rb
@@ -1,6 +1,26 @@
 # In this class, I'm gonna display the logic to make the game for the person
 class PlayerUser
 
+    def initial_colors
+        list_colors = ["verde","amarillo","rojo","azul"]
+    end
+
+    def 24_combinations_algorithm
+        
+        
+    end
+
+    def test
+        
+        hash= {
+            1=>{key:1,value:[1,2,3]},
+            2=>{key:2,value:[4,5,6]}
+        }
+        puts hash[1][:value]
+    end
 
     
 end
+
+pl= PlayerUser.new
+pl.test
